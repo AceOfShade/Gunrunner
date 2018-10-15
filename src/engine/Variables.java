f995b1fc417b23552fa59d747bd17f5f293eb24e
marcomitc <43962018+marcomitc@users.noreply.github.com>
2018-10-15 16:57:18 +0200

    unnötig

--- src/engine/Variables.java
+++ src/engine/Variables.java
@@ -17,20 +17,12 @@ public class Variables {
 	static int x=400,y=400;
 	
 	//Player
-<<<<<<< HEAD
-<<<<<<< HEAD
 	static int px=350,py=400; 
 	static BufferedImage phight;			//Player hight
 	static BufferedImage pwidth;			//Player width
 	static int kills=0;						//kills from player
 	static int health=3;					//player health
 	static ArrayList<Sprite> playerSprites;	//Arraylist with all objects from type Sprite
-=======
-	
->>>>>>> 63c300d27df33435d1313e6f4f4e6a7dfe0e28e4
-=======
-	
->>>>>>> 63c300d27df33435d1313e6f4f4e6a7dfe0e28e4
 	//Sound
 	static boolean sound=false;
 	//Enemy
