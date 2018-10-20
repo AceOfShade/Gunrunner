559a0a0e0dbe103d37f84d737bf757950a3ebade
HyFabi <43959083+HyFabi@users.noreply.github.com>
2018-10-20 17:44:12 +0200

    a

---
+++ src/main/cheatcodes.java
@@ -0,0 +1,22 @@
+package main;
+
+public class cheatcodes {
+	public static char[] lastPressed = new char[6];
+	private byte freiePos;
+	
+	public void p(char a) {
+		if(freiePos != lastPressed.length) {
+			lastPressed[freiePos] = a;
+			freiePos++;
+		}else {
+			for(int i = 0; i< lastPressed.length;i++) {
+				lastPressed[i]= lastPressed[i+1];
+				lastPressed[lastPressed.length] = a;
+			}
+		}
+	}
+	
+	public void cheat() {
+		
+	}
+}
