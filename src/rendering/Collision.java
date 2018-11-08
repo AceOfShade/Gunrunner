35b426bb7c4e36355691d7d9f4cd2426a15159d8
HyFabi <43959083+HyFabi@users.noreply.github.com>
2018-11-08 17:06:15 +0100

    d

--- src/rendering/Collision.java
+++ src/rendering/Collision.java
@@ -8,14 +8,6 @@ import entities.Enemy;
 public class Collision {
 
 	public void testC(){
-		for(int i = 0; i< Main.lvl.shotObjects.size(); i++) {
-			if(collisionRect(Main.lvl.player, Main.lvl.shotObjects.get(i))) {
-				System.out.println("C: S-E");
-				Enemy.bodycollision =true;
-			}
-			
-		}
-		
 		for(Enemy e : Main.lvl.enemyObjects) {
 			if(collisionRect(Main.lvl.player, e)) {
 				System.out.println("C: P-E");
@@ -25,29 +17,11 @@ public class Collision {
 		for(Shot e : Main.lvl.shotObjects) {
 			for(Enemy f : Main.lvl.enemyObjects){
 				if(collisionRect(f, e)) {
-					System.out.println("C: P-E");
+					System.out.println("C: S-E");
 				}
 			}
-			
 		}
-		
 	}
-	
-		
-//	public boolean rectangleRectangleCollision(GameObject a, GameObject b) {
-//		if (a.getX() + a.getW() <= b.getX() + b.getW()) { // +58 because of the detection area behind the enemy
-//			if (a.getX() + a.getX() >= b.getW()) { // +80 because of the detection area before the enemy
-//				if (a.getY() + a.getH() <= b.getY() + b.getH()) {
-//					if (a.getY() + a.getH() >= b.getY()) {
-//						System.out.println("C");
-//						return true;
-//					}
-//				}
-//			}
-//		}
-//
-//		return false;
-//	}
 	
 	public boolean collisionRect(GameObject r1, GameObject r2){
 	    return r1.y < r2.y + r2.h && r1.y + r1.h > r2.x && r1.x + r1.w > r2.x && r1.x < r2.x + r2.w;
--- src/rendering/KeyHandler.java
+++ src/rendering/KeyHandler.java
@@ -3,11 +3,14 @@ package rendering;
 import java.awt.event.KeyEvent;
 
 import java.awt.event.KeyListener;
+import java.util.Scanner;
+
 import main.Variables;
 
+/** sieg heil */
 public class KeyHandler implements KeyListener {
+	public static char lastPressed;
 	public static boolean wPressed, aPressed, dPressed, spacePressed, f2Pressed, f1Pressed, escPressed;
-
 	@Override
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_W) {
@@ -31,56 +34,7 @@ public class KeyHandler implements KeyListener {
 			f1Pressed = true;
 		}
 		if (e.getKeyCode() == KeyEvent.VK_F2) {
-			if (f2Pressed) {
-				f2Pressed = false;
-			} else {
-				f2Pressed = true;
-			}
-		}
-		switch(e.getKeyCode()) {
-		case KeyEvent.VK_F1: break;
-		case KeyEvent.VK_F2: break;
-		case KeyEvent.VK_F3: break;
-		case KeyEvent.VK_F4: break;
-		case KeyEvent.VK_F5: break;
-		case KeyEvent.VK_F6: break;
-		case KeyEvent.VK_F7: break;
-		case KeyEvent.VK_F8: break;
-		case KeyEvent.VK_F9: break;
-		case KeyEvent.VK_F10: break;
-		case KeyEvent.VK_F11: break;
-		case KeyEvent.VK_F12: break;
-		case KeyEvent.VK_Q: break;
-		case KeyEvent.VK_W: break;
-		case KeyEvent.VK_E: break;
-		case KeyEvent.VK_R: break;
-		case KeyEvent.VK_T: break;
-		case KeyEvent.VK_Z: break;
-		case KeyEvent.VK_U: break;
-		case KeyEvent.VK_I: break;
-		case KeyEvent.VK_O: break;
-		case KeyEvent.VK_P: break;
-		case KeyEvent.VK_A: break;
-		case KeyEvent.VK_S: break;
-		case KeyEvent.VK_D: break;
-		case KeyEvent.VK_F: break;
-		case KeyEvent.VK_G: break;
-		case KeyEvent.VK_H: break;
-		case KeyEvent.VK_J: break;
-		case KeyEvent.VK_K: break;
-		case KeyEvent.VK_L: break;
-		case KeyEvent.VK_Y: break;
-		case KeyEvent.VK_X: break;
-		case KeyEvent.VK_C: break;
-		case KeyEvent.VK_V: break;
-		case KeyEvent.VK_B: break;
-		case KeyEvent.VK_N: break;
-		case KeyEvent.VK_M: break;
-		case KeyEvent.VK_SPACE: break;
-		case KeyEvent.VK_SHIFT: break;
-		case KeyEvent.VK_ESCAPE: 
-			escPressed=true;
-			break;
+			f2Pressed = !f2Pressed;
 		}
 	}
 
