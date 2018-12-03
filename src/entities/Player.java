1a75987096a063089532c2fc0b9f388733c8059c
HyFabi <43959083+HyFabi@users.noreply.github.com>
2018-12-03 14:54:02 +0100

    :trollface:

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -5,16 +5,19 @@ public class Player extends GameObject {
 	public int health; // player health
 	int velY;
 	public static int jumpheight = 70;
-
+	public String waffe;
+	
 	public Player(int x, int y) {
 		super(x, y, 100, 140);
 		this.health = 3;
 		velY = 0;
+		waffe = "Sturmgewehr";
 	}
 	
-	public Player(int x, int y, byte health) {
+	public Player(int x, int y, byte health,String w) {
 		super(x, y, 140, 100);
 		this.health = health;
+		waffe = w;
 	}
 
 	public void render() {
