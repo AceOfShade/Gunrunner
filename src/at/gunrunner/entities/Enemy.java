0c0aaaa9c19fcfe75e5f1d4b41dd2ff1d096a4ce
HyFabi <43959083+HyFabi@users.noreply.github.com>
2019-03-03 11:17:57 +0100

    Revert "Revert "Levelsystem erstellt""
    This reverts commit c7e4cf866a5ae224767373ad633f0d2d5ed396b8.

--- src/at/gunrunner/entities/Enemy.java
+++ src/at/gunrunner/entities/Enemy.java
@@ -33,7 +33,7 @@ public class Enemy extends GameObject {
 			if(health == 0) {
 				enemydead = true;
 				Main.gw.player.kills++;
-				Main.gw.idks.add(new HitMarker(x, y, false));
+				Main.gw.hitmarker.add(new HitMarker(x, y, false));
 			}
 		}
 	}
